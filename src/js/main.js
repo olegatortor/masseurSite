cc8382badee0d8bf75594b00adab4382679b16c2
Oleg <olegatortor111@gmail.com>
2023-07-12 00:04:01 +0300

    create timer

--- src/js/main.js
+++ src/js/main.js
@@ -41,4 +41,65 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
+
+
+    function getTimeRemaining(endtime) {
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endtime) - Date.parse(new Date());
+
+        if (t <= 0) { 
+                days = 0,
+                hours = 0,
+                minutes = 0,
+                seconds = 0;
+        } else {
+                days = Math.floor(t / (1000 * 60 * 60 * 24)),
+                hours = Math.floor(t / (1000 * 60 * 60) % 24),
+                minutes = Math.floor((t / 1000 / 60) % 60),
+                seconds = Math.floor((t / 1000) % 60);
+        }
+
+        
+        
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        }
+    }
+
+    function addZero(item) {
+        if(item < 10) {
+            return `0${item}`
+        } else {
+            return item;
+        }
+    }
+
+    function setClocks(selector, deadline) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            const t = getTimeRemaining(deadline);
+
+            days.innerHTML = addZero(t.days);
+            hours.innerHTML = addZero(t.hours);
+            minutes.innerHTML = addZero(t.minutes);
+            seconds.innerHTML = addZero(t.seconds);
+
+            if(t.total === 0) {
+                clearInterval(updateClock)
+            }
+        }
+    }   
+
+    setClocks('.signup__timer', new Date(2023, 7, 10));
 })
